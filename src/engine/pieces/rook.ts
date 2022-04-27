1ff72eb289cd2ab0d598243c39a02475107fe7e2
Travis Woodward <Travis.Woodward@softwire.com>
2022-04-27 13:19:34 +0100

    Passes Red-3

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     constructor(player: Player) {
@@ -8,6 +10,15 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare: Square = board.findPiece(this);
+        const availableRowMoves: Square[] = new Array(GameSettings.BOARD_SIZE)
+            .fill(undefined)
+            .map((_, rowNumber: number) => new Square(rowNumber, currentSquare.col))
+            .filter((square: Square) => square.row !== currentSquare.row);
+        const availableColMoves = new Array(GameSettings.BOARD_SIZE)
+            .fill(undefined)
+            .map((_, colNumber: number) => new Square(currentSquare.row, colNumber))
+            .filter((square: Square) => square.col !== currentSquare.col);
+        return availableRowMoves.concat(availableColMoves);
     }
 }
