9ab2094db33457101d26e26a094930317460e22a
Travis Woodward <Travis.Woodward@softwire.com>
2022-04-27 12:02:14 +0100

    Passes Red-1

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,13 +1,18 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import Square from "../square";
 
 export default class Pawn extends Piece {
     constructor(player: Player) {
         super(player);
     }
 
-    getAvailableMoves(board: Board) {
-        return new Array(0);
+    getAvailableMoves(board: Board): Square[] {
+        const currentSquare = board.findPiece(this);
+        const newRow = this.player === Player.WHITE
+            ? currentSquare.row + 1
+            : currentSquare.row - 1;
+        return [Square.at(newRow, currentSquare.col)];
     }
 }
