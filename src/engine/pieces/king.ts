35134be7206ea699d68c6e76321e60327bc2919d
Travis Woodward <Travis.Woodward@softwire.com>
2022-04-27 15:39:55 +0100

    Passes Red-7

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,13 +1,16 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import Square from "../square";
 
 export default class King extends Piece {
     constructor(player: Player) {
         super(player);
     }
 
-    getAvailableMoves(board: Board) {
-        return new Array(0);
+    canMoveFromTo(fromSquare: Square, toSquare: Square): boolean {
+        return (fromSquare.isVerticalTo(toSquare) && Math.abs(fromSquare.verticalDistanceTo(toSquare)) === 1)
+            || (fromSquare.isHorizontalTo(toSquare) && Math.abs(fromSquare.horizontalDistanceTo(toSquare)) === 1)
+            || (fromSquare.isDiagonalTo(toSquare) && fromSquare.manhattanDistanceTo(toSquare) === 2);
     }
 }
